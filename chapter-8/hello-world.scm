51dfd154891273aa043907553841d8f5075d26f3
rhmtrz <2249301106@qq.com>
2018-07-05 09:17:57 +0900

    chore: fix indentand typo

--- chapter-8/hello-world.scm
+++ chapter-8/hello-world.scm
@@ -1,8 +1,8 @@
 (print ((lambda (x) x) "Hello World"))
 
 (define my-name
+  (lambda (name)
     (lambda (name)
-        (lambda (name)
-            (cons name '()))))
+      (cons name '()))))
 
 (print (my-name "Rahmat"))
--- chapter-8/rember-f.scm
+++ chapter-8/rember-f.scm
@@ -1,12 +1,11 @@
 
-
 (define rember-f
-    (lambda (test?)
-        (lambda (a lat)
-            (cond 
-                ((null? lat) '())
-                ((test? a (car lat)) (cdr lat))
-                (else (cons (car lat)
-                    ((rember-f test?) a (cdr lat))))))))
+  (lambda (test?)
+    (lambda (a lat)
+      (cond 
+        ((null? lat) '())
+        ((test? a (car lat)) (cdr lat))
+        (else (cons (car lat)
+          ((rember-f test?) a (cdr lat))))))))
 
 (print ((rember-f eq?) 'a '(b a c)))
