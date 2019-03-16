2d06a805f7e61fe771cc1d840dfc077a1b213569
rhmt <rezaeihazara@gmail.com>
2019-03-16 19:09:16 +0900

    fix: rm-even* function

--- practice-func/rm-even*.scm
+++ practice-func/rm-even*.scm
@@ -2,14 +2,40 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-(print (atom? '()))
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define multiple
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (+ n (multiple n (sub1 m)))))))
+
+(print "------multiple")
+(print (multiple 5 3))
+
+(define divid
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (divid (- n m) m))))))
+
+
+(print "------divid")
+(print (divid 23 4))
 
 (define even?
   (lambda (n)
-    (= (* (/ n 2) 2) n)))
+    (= (multiple (divid n 2) 2) n)))
 
+(print (even? 7))
+(print (even? 12))
 
-(print (even? 11))
 
 (define evens-only*
   (lambda (l)
@@ -36,10 +62,10 @@
           ((even? (car ls)) (rm-even* (cdr ls)))
           (else
             (cons (car ls) (rm-even* (cdr ls))))))
-      (else (cons (car ls) (cdr ls))))))
+      (else (cons (rm-even* (car ls)) (rm-even* (cdr ls)))))))
 
-(print (rm-even* '(1 2 3 4)))
+(print (rm-even* '(1 2 3 4 5 6)))
 
-(print (rm-even* '(1 2 (3 4 5) 3 4)))
+(print (rm-even* '(1 2 (3 4 5) 6 7)))
 
-(print (rm-even* '(1 (2 (3 4 5) 3 4))))
+(print (rm-even* '(1 (2 (3 4 5) 6 7))))
